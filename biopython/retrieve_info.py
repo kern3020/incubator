b5895111f9fc2c80b0b9e4c19d44510616d05013
John Kern <kern3020@gmail.com>
2015-01-11 18:55:20 -0800

    pep8 cleanup

--- biopython/retrieve_info.py
+++ biopython/retrieve_info.py
@@ -6,6 +6,7 @@ from Bio import Entrez
 if 'ENTREZ_EMAIL' in os.environ:
     Entrez.email = os.environ['ENTREZ_EMAIL']
 
+
 def retrieve_info(gene_syms, search_pat, bed_file, notes_file):
     '''given a list of gene_syms and file handles for output, retrieve the
     location and description. Location information is saved in bed
@@ -24,12 +25,14 @@ def retrieve_info(gene_syms, search_pat, bed_file, notes_file):
                 chromo = summary[0]['Chromosome']
                 start = summary[0]['GenomicInfo'][0]['ChrStart']
                 stop = summary[0]['GenomicInfo'][0]['ChrStop']
-                bed_file.write('{0}\t{1}\t{2}\t{3}\n'.format(chromo, start, stop, gene))
+                bed_file.write('{0}\t{1}\t{2}\t{3}\n'.format(
+                    chromo, start, stop, gene))
 
                 # descriptions
                 desc_short = summary[0]['Description']
                 desc_long = summary[0]['Summary']
-                notes_file.write('{0}(chr: {1}) - {2}\n'.format(gene, chromo, desc_short))
+                notes_file.write('{0}(chr: {1}) - {2}\n'.format(
+                    gene, chromo, desc_short))
                 notes_file.write('Summary: {0}\n\n'.format(desc_long))
 
 
@@ -42,10 +45,13 @@ def get_cancer_normal():
         os.remove(notes)
     with open(bed, 'w+') as bed_file:
         with open(notes, 'w+') as notes_file:
-            gene_list = ['APC', 'FBXW7', 'GNAS', 'KRAS', 'MAFB','SRC', 
+            gene_list = ['APC', 'FBXW7', 'GNAS', 'KRAS', 'MAFB', 'SRC',
                          'SMAD4', 'TP53', 'TTN']
             # gene_list = ['APC']
-            retrieve_info(gene_list,"{0}[gene] AND Homo sapiens[organism]",  bed_file, notes_file)
+            retrieve_info(gene_list,
+                          "{0}[gene] AND Homo sapiens[organism]",
+                          bed_file, notes_file)
+
 
 def get_yeast():
     yeast_bed = 'yeast.bed'
@@ -62,7 +68,10 @@ def get_yeast():
                    'MET5', 'IML1', 'HIR3', 'PMT4', 'DAN4']
     with open(yeast_bed, 'w+') as bed_file:
         with open(yeast_notes, 'w+') as notes_file:
-            retrieve_info(yeast_genes, "{0}[gene] AND Saccharomyces cerevisiae[organism]", bed_file, notes_file)
+            retrieve_info(
+                yeast_genes,
+                "{0}[gene] AND Saccharomyces cerevisiae[organism]",
+                bed_file, notes_file)
 
 if __name__ == "__main__":
     get_yeast()
